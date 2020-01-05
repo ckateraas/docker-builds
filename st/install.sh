bdaa3118680b3d48eb98186af1e06a7af2bf027a
Christian <christian@example.no>
2020-01-05 14:43:14 +0100

    Update install script for st

--- st/install.sh
+++ st/install.sh
@@ -4,17 +4,16 @@ set -e
 
 PACKAGE_NAME="st"
 IMAGE_NAME="$PACKAGE_NAME-builder"
-BUILD_DIR="/dist"
+BUILD_DIR="dist"
 source ../util.sh
 
 git-clone https://github.com/lukesmithxyz/st
 git-pull
 
 echo "Overiding config.h in $PACKAGE_NAME/"
-cp ./config.h "$PACKAGE_NAME"
+cp ./config.h "$GIT_REPO_DIR"
 
 bump-package-version "$PACKAGE_NAME"
-clean-build-dir "$BUILD_DIR"
 build-docker-image "$IMAGE_NAME"
 build-package "$IMAGE_NAME"
 install-package "$BUILD_DIR"
