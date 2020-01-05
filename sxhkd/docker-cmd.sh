fb7792f815839abe1b460f78ce35c43adea4ba7d
Christian <christian@example.no>
2020-01-05 14:20:34 +0100

    Add setup to build sxhkd

---
+++ sxhkd/docker-cmd.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+set -e
+
+make
+
+echo 'sxhkd' > description-pak
+
+checkinstall \
+  --type=debian \
+  --maintainer=hvemvet \
+  --nodoc \
+  --pkgname=st \
+  --pkgversion="git describe --tags 2> /dev/null" \
+  --default \
+  --pakdir=./dist \
+  --install=no \
+  --replaces='sxhkd'
